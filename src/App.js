3330208432fa6fa85ef045a8d96bf046c9689717
swastikdayma <128327129+swastikdayma@users.noreply.github.com>
2023-08-25 18:03:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import {useState} from 'react';
+import { Form } from "./components/Form";
+import { Todos } from "./components/Todos";
+import {useDispatch, useSelector} from 'react-redux';
+import {deleteAll} from './redux/todoapp/actions';
+
+function App() {
+  
+const dispatch = useDispatch();
+const todos = useSelector((state)=>state.operationsReducer);
+const [editFormVisibility, setEditFormVisibility]=useState(false);
+const [editTodo, setEditTodo]=useState('');
+const handleEditClick=(todo)=>{
+    setEditFormVisibility(true);
+    setEditTodo(todo);
+  }
+ const cancelUpdate=()=>{
+    setEditFormVisibility(false);
+  }
+
+    return (
+    <div className="wrapper">
+      <br></br>
+      <h1 className="text-center">TO-DO LIST</h1>
+      <Form editFormVisibility={editFormVisibility} editTodo={editTodo}
+      cancelUpdate={cancelUpdate}/>
+      <Todos handleEditClick={handleEditClick} editFormVisibility={editFormVisibility}/>
+      {todos.length > 1 && (
+        <button className='btn btn-danger btn-md delete-all'
+        onClick={()=>dispatch(deleteAll())}>Delete All</button>
+      )}
+    </div>
+  );
+}
+
+export default App;
